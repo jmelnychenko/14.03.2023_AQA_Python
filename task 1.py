b2e7a3aa0d73a2290e10b767cac368b462080bed
Julia Melnichenko <ju.melnichenko@gmail.com>
2023-03-22 21:00:45 +0200

    task1

---
+++ task 1.py
@@ -0,0 +1,42 @@
+# Task 1.1
+
+first = 10
+second = 30
+
+sum_of_values = first + second
+print(sum_of_values)
+
+subtraction_of_values = first - second
+print(subtraction_of_values)
+
+multiplication_of_values = first * second
+print(multiplication_of_values)
+
+division_of_values = first / second
+print(division_of_values)
+
+exponentiation_of_values = first ** second
+print(exponentiation_of_values)
+
+division2_of_values = first // second
+print(division2_of_values)
+
+# Task 1.2
+
+bool1 = first > second
+print(bool1)
+
+bool2 = first < second
+print(bool2)
+
+bool3 = first == second
+print(bool3)
+
+bool4 = first != second
+print(bool4)
+
+# Task 1.3
+
+hello_value = "Hello "
+world_value = "world!"
+print(hello_value + world_value)
