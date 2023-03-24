6a4127954741ef38b12d12a185c1ff91530406be
Julia Melnichenko <ju.melnichenko@gmail.com>
2023-03-24 16:36:10 +0200

    task2 updated

--- task2.py
+++ task2.py
@@ -1,17 +1,18 @@
-# Task 2.1
-user_input = input('Input: ')
-print("Word " + user_input + " has " + str(len(user_input)) + " letters.")
+# # Task 2.1
+# user_input = input('Input: ')
+# print("Word {0} has {1} letters.".format(user_input, str(len(user_input))))
 
 # Task 2.2 - Касир в кінотеватрі
 user_age = input('Enter your age: ')
-if user_age.isdigit():
-    if 7 > int(user_age) > 0:
+ua_var = int(user_age)  #user_age_variable
+if type(ua_var) == int:
+    if 7 > ua_var > 0:
         print("Де твої батьки")
-    elif int(user_age) == 7:
+    elif ua_var == 7:
         print("Вам сьогодні пощастить!")
-    elif 16 > int(user_age) > 7:
+    elif 16 > ua_var > 7:
         print("Це фільм для дорослих!")
-    elif 99 > int(user_age) >= 65:
+    elif 99 > ua_var >= 65:
         print("Покажіть пенсійне посвідчення!")
     else:
         print("А білетів вже немає!")
