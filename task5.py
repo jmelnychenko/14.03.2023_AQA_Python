ce9b9cf5b914a4e1f84dd9871160cfe93bf7f2be
Julia Melnichenko <ju.melnichenko@gmail.com>
2023-05-12 23:01:38 +0300

    task 5

---
+++ task5.py
@@ -0,0 +1,35 @@
+import null as null
+import requests
+
+
+url = "https://dummyjson.com/users?limit=100"
+
+response = requests.get(url)
+
+my_users = response.json()
+
+list_of_men = my_users['users']
+brown_man_age = 0
+brown_man_count = 0
+browm_avarage_age = 0
+list_Louisville = []
+
+# середній вік чоловіків з Brown волоссям
+for man in list_of_men:
+    man_age = man.get('age', 0)
+    man_hair = man.get('hair')['color']
+
+    if man_hair == "Brown":
+        brown_man_age += man_age
+        brown_man_count += 1
+        brown_avarage_age = brown_man_age/brown_man_count
+print(int(brown_avarage_age))
+
+#список людей, що проживають в місті Louisville
+list_Louisville = []
+for man in list_of_men:
+    man_city = man.get('address')['city']
+    if man_city == 'Louisville':
+        list_Louisville.append(man)
+print(list(list_Louisville))
+
